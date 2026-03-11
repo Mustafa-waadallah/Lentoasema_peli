bd2e8e4a1d3b195099c923d9f213feb181d57ce8
Grfrf <grfrf7767@gmail.com>
2026-03-11 16:06:41 +0200

    Aloitus

--- lentoasema_peli.py
+++ lentoasema_peli.py
@@ -7,8 +7,10 @@ print(" Jokainen lentoasema tuottaa pelaajalle rahaa.")
 print("Pelaajan tavoitteena on kerätä yhteensä 1000 euroa voittaakseen pelin.")
 print("Jos pelaajan rahat putoavat nollaan, peli päättyy häviöön.")
 
-#Pelin aloitus
-aloitus = input("Oletko pelannut tätä peliä aiemmin? Vastaa: joo tai ei.")
+aloitus = input("Oletko pelannut tätä peliä aiemmin? Vastaa: joo tai ei. ")
+while aloitus != "joo" and aloitus != "ei":
+    print("Yritä uudelleen, kirjoita joo tai ei")
+    aloitus = input("Oletko pelannut tätä peliä aiemmin? Vastaa: joo tai ei. ")
 
 #kirjautuminen
 
@@ -27,33 +29,41 @@ def tallentaminen(username, password):
 
 def kirjautuminen():
     print("Tervetuloa kirjautumaan!")
-    username = []
-    password = []
 
-    username.append(username1)
-    password.append(password1)
+    yhteys = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="2006",
+        database="logins"
+    )
+    kursori = yhteys.cursor()
 
-    username2 = input("Kirjoita käyttäjätunnuksesi: ")
-    if username2 in username:
-            password2 = input("Kirjoita salasana: ")
-    while username2 not in username:
-        print ("Käyttäjätunnus on väärin yritä uudelleen")
+    while True:
         username2 = input("Kirjoita käyttäjätunnuksesi: ")
-        if username2 in username:
-            password2 = input("Kirjoita salasana: ")
-            if password2 not in password:
-                print("salasna on väärin, yritä uudelleen!")
-                password2 = input("Kirjoita salasana: ")
-        if username2 in username and password2 in password:
-            print("Tervetuloa peliin!!")
-            tallentaminen(username2, password2)
+        kursori.execute("SELECT password FROM users WHERE username = %s", (username2,))
+        tulos = kursori.fetchone()
 
+        if tulos is None:
+            print("Käyttäjätunnus on väärin, yritä uudelleen")
+            continue
+
+        password2 = input("Kirjoita salasana: ")
+        if password2 == tulos[0]:
+            print("Tervetuloa peliin!!")
+            break
+        else:
+            print("Salasana on väärin, yritä uudelleen!")
+
+    kursori.close()
+    yhteys.close()
 #ensimmäinen kertaa pelaajalla
 
 if aloitus == "ei":
     username1 = input("Anna uusi käyttäjätunnus: ")
     password1 = input("Anna uusi salasana: ")
     tallentaminen(username1, password1)
+    kirjautuminen()
+elif aloitus == "joo":
     kirjautuminen()
 
 
@@ -66,7 +76,7 @@ nimimerkki.append(nimi)
 print ("Hello "+ nimi + "!")
 
 #pelaajan rahaa
-raha = 50
+raha = 100
 
 print("Kirjoita raha, jotta näet rahasi summan. Kun olet valmis, kirjoita lopeta.")
 print("Tämän jälkeen näet seuraavan tiedon:")
@@ -86,7 +96,7 @@ print("Kirjoita kauppa nähdääksesi lentoasemien kauppa")
 
 
 def kauppa():
-
+    global raha
     pelaajan_lentoasemat = []
 
     print("Osta lentoasema tai hylkää.")
@@ -98,17 +108,83 @@ def kauppa():
     if lentoasemat == "hylkkä":
         print (nimimerkki, "omistaa", pelaajan_lentoasemat)
 
+#Ensimmäinen lentoasema
     if lentoasemat == "yksityinen lentoasema":
         print("Lentoaseman hinta on 100 euro")
+        ostaminen = input("Kirjoita 'osta' ostaaksesi lentoaseman: ")
+        if ostaminen == "osta" and raha >= 100:
+            print("yksityinen lentoasema on ostettu.")
+            raha -= 100
+            print("Raha jäljellä: ", raha)
+            input("Kirjoita lentoaseman nimi että pystyt raha tienata: ")
+            while True:
+                time.sleep(1)
+                raha += 0.05
+                print("Rahasi:", (raha))
+                lopeta = input()
+                if lopeta == "lopeta":
+                    break
+        elif raha < 100:
+            print("Sinulla ei ole riittävä raha")
 
+#toinen lentoasema
     if lentoasemat == "sotilaslentoasema":
         print("Lentoaseman hinta on 100 euro")
+        ostaminen = input("Kirjoita 'osta' ostaaksesi lentoaseman: ")
+        if ostaminen == "osta" and raha >= 100:
+            print("Sotilas lentoasema on ostettu.")
+            raha -= 100
+            print("Raha jäljellä: ", raha)
+            input("Kirjoita lentoaseman nimi että pystyt raha tienata: ")
+            while True:
+                time.sleep(1)
+                raha += 0.05
+                print("Rahasi:", (raha))
+                lopeta = input()
+                if lopeta == "lopeta":
+                    break
+        elif raha < 100:
+            print("Sinulla ei ole riittävä raha")
+
+#Kolmas lentoasema
 
     if lentoasemat == "turistilentoasema":
         print("Lentoaseman hinta on 100 euro")
+        ostaminen = input("Kirjoita 'osta' ostaaksesi lentoaseman: ")
+        if ostaminen == "osta" and raha >= 100:
+            print("turistilentoasema on ostettu.")
+            raha -= 100
+            print("Raha jäljellä: ", raha)
+            input("Kirjoita lentoaseman nimi että pystyt raha tienata: ")
+            while True:
+                time.sleep(1)
+                raha += 0.05
+                print("Rahasi:", (raha))
+                lopeta = input()
+                if lopeta == "lopeta":
+                    break
+        elif raha < 100:
+            print("Sinulla ei ole riittävä raha")
+
+#Neljäs lentoasema
 
     if lentoasemat == "älylentoasema":
         print("Lentoaseman hinta on 100 euro")
+        ostaminen = input("Kirjoita 'osta' ostaaksesi lentoaseman: ")
+        if ostaminen == "osta" and raha >= 100:
+            print("älylentoasema on ostettu.")
+            raha -= 100
+            print("Raha jäljellä: ", raha)
+            input("Kirjoita lentoaseman nimi että pystyt raha tienata: ")
+            while True:
+                time.sleep(1)
+                raha += 0.05
+                print("Rahasi:", (raha))
+                lopeta = input()
+                if lopeta == "lopeta":
+                    break
+        elif raha < 100:
+            print("Sinulla ei ole riittävä raha")
 
 while True:
     lentoasemat = input()
@@ -116,17 +192,34 @@ while True:
         kauppa()
         break
 
-
 #pelaaja kerää rahaa siivouksesta
-print("jos rahat eivät riittää, kerää ne siivouksesta.")
+print("jos rahat eivät riittää tai haluat tienata enemmän, kerää ne siivouksesta.")
 print("saat 0.025 euroa sekunnista")
-siivous = input("kirjoita siivuos sana aloittaaksesi siivous")
+siivous = input("kirjoita siivous sana aloittaaksesi siivous")
 
 if siivous == "siivous":
-    print("Siivous alkoi! Kirjoita 'lopeta' lopettaaksesi.")
+    print("Siivous alkoi! Kirjoita 'lopeta' lopettaaksesi: ")
     while True:
         time.sleep(1)
-        raha += 0.025
+        raha += 0.05
         print("Rahasi:", (raha))
-        if siivous == "lopeta":
+        lopeta = input()
+        if lopeta == "lopeta":
             break
+while True:
+    kertominen = input("Kirjoita komento: ")
+
+    if kertominen == "raha":
+        print("Sinulla on", raha, "euroa")
+
+    if kertominen == "kauppa":
+        kauppa()
+
+    if kertominen == "siivous":
+        print("Siivous alkoi!")
+        while True:
+            time.sleep(1)
+            raha += 0.05
+            print("Rahasi:", raha)
+            if input() == "lopeta":
+                break
